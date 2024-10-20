89e38192177e0ee665f1fd7d1c2acc8ef4157884
Ben Povar <ben.povar@warwick.ac.uk>
2024-10-20 13:39:57 +0100

    Computing derivatives of the laplace transform of the Linnik RV.

--- branching_processes_simulation/src/branching_processes_simulation/linnik.py
+++ branching_processes_simulation/src/branching_processes_simulation/linnik.py
@@ -1,8 +1,35 @@
 import numpy as np
-from scipy.stats import gengamma
+from sympy import symbols, diff, Function
 
 from branching_processes_simulation.random_variable import RandomVariable
 from branching_processes_simulation.stable_random_variable import StableRandomVariable
+
+
+
+class LinnikLaplaceTransform():
+    def __init__(self, alpha: float, beta: float) -> None:
+        x = symbols('x')
+        self._f = 1 / (1 + x**alpha) ** beta
+        self._x = x
+        self._alpha = alpha
+        self._beta = beta
+        self._der_f_map = {0: self._f}
+
+    def get_kth_derivative(self, k: int) -> Function:
+        if k in self._der_f_map:
+            return self._der_f_map[k]
+        
+        j = k-1
+        while j not in self._der_f_map:
+            --j
+        
+        der_f_k = diff(self._der_f_map[j], self._x, k - j)
+        self._der_f_map[k] = der_f_k
+        return der_f_k
+    
+    def get_kth_derivative_at_x(self, k: int, x: np.float64) -> np.float64:
+        der_f_k = self.get_kth_derivative(k)
+        return der_f_k.subs(self._x, x)
 
 
 class Linnik(RandomVariable):
@@ -11,12 +38,13 @@ class Linnik(RandomVariable):
         self.beta = beta
         # self._v = gengamma(self.delta, 1 / self.delta)
         self._s = StableRandomVariable(self.alpha)
+        self._laplace_transform = LinnikLaplaceTransform(alpha, beta)
 
     def characteristic_function(self, t: np.complex64) -> np.complex64:
-        return 1 / np.power((1 + np.power(np.abs(t), self.alpha)), self.beta)
+        return self._laplace_transform.get_kth_derivative_at_x(0, t)
 
     def laplace_transform(self, t: np.float64) -> np.float64:
-        return np.real(self.characteristic_function(t))
+        return self._laplace_transform.get_kth_derivative_at_x(0, t)
 
     def pdf(self, x: np.float64) -> np.float64:
         return None
@@ -24,16 +52,16 @@ class Linnik(RandomVariable):
     def cdf(self, x: np.float64) -> np.float64:
         return None
     
-    #TODO: check
-    def mean(self, t: np.float64) -> np.float64:
-        return np.infty
+    def mean(self) -> np.float64:
+        return np.inf if self.alpha < 1 else self.beta
 
-    #TODO: check
-    def variance(self, t: np.float64) -> np.float64:
-        return np.infty
+    def variance(self) -> np.float64:
+        return np.inf if self.alpha < 1 else self.beta
 
     def sample(self, N: int) -> np.ndarray[float]:
         v = self.rng.gamma(self.beta, 1, N)
         s = self._s.sample(N)
         return np.power(v, 1/self.alpha) * s
     
+    def laplace_transform_kth_derivative(self, k: int, t: np.float64) -> np.float64:
+        return (-1)**k * self._laplace_transform.get_kth_derivative_at_x(k, t)
---
+++ branching_processes_simulation/tests/test_laplace_transform.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import time
+
+from branching_processes_simulation.discrete_space_process.bgw import BGW
+from branching_processes_simulation.discrete_space_process.bgwi import BGWI
+from branching_processes_simulation.discrete_space_process.immigration_exp_rv import ImmigrationExpRandomVariable
+from branching_processes_simulation.discrete_space_process.reproduction_exp_rv import ReproductionExpRandomVariable
+from branching_processes_simulation.linnik import Linnik
+
+def test():
+    alpha = 0.4
+    c = 0.4
+    d = 0.2
+    delta = d / (alpha * c)
+
+    L = Linnik(alpha, delta)
+    print(L.sample(1))
+    print(L.laplace_transform_kth_derivative(1, 1))
+    
