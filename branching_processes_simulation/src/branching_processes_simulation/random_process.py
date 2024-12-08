03a39b8c3d5e47588683996c6ebd6230a9ac351e
Ben Povar <ben.povar@warwick.ac.uk>
2024-12-08 15:51:31 +0000

    Extended sampling logic.

--- branching_processes_simulation/src/branching_processes_simulation/random_process.py
+++ branching_processes_simulation/src/branching_processes_simulation/random_process.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Callable, List
 import numpy as np
 
 from branching_processes_simulation.i_random import IRandom
@@ -26,9 +26,20 @@ class RandomProcess(IRandom):
         return None
 
     @abstractmethod
-    def sample(self, N: int, time: float, z: np.float64) -> np.ndarray[float]:
-        return None
+    def sample(self, N: int, times: List[float], z: np.float64, function:Callable=None, **kwargs) -> np.ndarray[np.ndarray[float]]:
+        if len(times) == 1:
+            res = np.array([self.sample_on_time(N, times[0], z, **kwargs)])
+        else:
+            res = self.sample_on_times(N, times, z, **kwargs)
+        
+        if function is not None:
+            for i in range(len(times)):
+                res[i] = function(res[i], t=times[i])
+        return res
+
+    def sample_on_time(self, N: int, time: float, z: np.float64, **kwargs) -> np.ndarray[float]:
+        return self.sample_on_times(N, [time], z, **kwargs).flatten()
     
     @abstractmethod
-    def sample_function(self, N: int, theta: Callable, time: float, z: np.float64) -> np.ndarray[float]:
+    def sample_on_times(self, N: int, times: List[float], z: np.float64, **kwargs) -> np.ndarray[np.ndarray[float]]:
         return None
