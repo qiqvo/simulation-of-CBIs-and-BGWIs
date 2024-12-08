95b9256fb8dd476c109862a89e2501ae94ed9e87
Ben Povar <ben.povar@warwick.ac.uk>
2024-12-08 14:51:46 +0000

    Checked mean and variance.

--- branching_processes_simulation/src/branching_processes_simulation/continuous_space_process/polya_transformed_tau.py
+++ branching_processes_simulation/src/branching_processes_simulation/continuous_space_process/polya_transformed_tau.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import betaprime
+from scipy import integrate
 
 from branching_processes_simulation.random_variable import RandomVariable
 
@@ -22,13 +23,22 @@ class PolyaTransformedTau(RandomVariable):
     def cdf(self, x: np.float64) -> np.float64:
         return x**(1 + self.alpha)/(1 + x**self.alpha)**((1 + self.alpha)/self.alpha)
 
-    #TODO: check
     def mean(self) -> np.float64:
         return np.infty
 
-    #TODO: check
     def variance(self) -> np.float64:
         return np.infty
+    
+    def fractional_moment(self, beta:float) -> np.float64:
+        if beta >= self.alpha: 
+            return np.inf
+        else:
+            p1 = integrate.quad(lambda x: self.pdf(x) * x**beta, 0, 1)
+            p2 = (1 + self.alpha) * integrate.quad(
+                lambda x: x**(- beta/self.alpha) / (1 + x)**(2 + 1/self.alpha), 
+                0, 1
+            )
+            return p1 + p2 
 
     def sample(self, N: int) -> np.ndarray[float]:
         x = self._x.rvs(N)
