fb84eac33a221f5c9781124778230fab9f09c3be
Ben Povar <ben.povar@warwick.ac.uk>
2024-12-08 14:14:35 +0000

    Stub for a test.

---
+++ branching_processes_simulation/tests/test_sampling_of_tau.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import time
+
+from branching_processes_simulation.discrete_space_process.bgw import BGW
+from branching_processes_simulation.discrete_space_process.bgwi import BGWI
+from branching_processes_simulation.discrete_space_process.immigration_exp_rv import ImmigrationExpRandomVariable
+from branching_processes_simulation.discrete_space_process.reproduction_exp_rv import ReproductionExpRandomVariable
+
+def test():
+    # TODO: sample function: 
+    return None
+
+# if __name__ == '__main__':
+#     main()
