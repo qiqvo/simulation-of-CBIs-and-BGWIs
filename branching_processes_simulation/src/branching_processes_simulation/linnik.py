bd774495759bb58dff326b56a302f3d721050859
Ben Povar <ben.povar@warwick.ac.uk>
2024-12-10 23:43:54 +0000

    Polya sampling.

--- branching_processes_simulation/src/branching_processes_simulation/linnik.py
+++ branching_processes_simulation/src/branching_processes_simulation/linnik.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sympy import symbols, diff, Function
 
+from branching_processes_simulation.continuous_space_process.fejer_de_la_vallee_poussin_random_variable import FejerDeLaValleePoussinRandomVariable
 from branching_processes_simulation.exponential import Exponential
 from branching_processes_simulation.random_variable import RandomVariable
 from branching_processes_simulation.stable_random_variable import PositiveStableRandomVariable
@@ -48,6 +49,7 @@ class Linnik(RandomVariable):
         self.alpha = alpha
         self.beta = beta
         # self._v = gengamma(self.delta, 1 / self.delta)
+        self._fvp = FejerDeLaValleePoussinRandomVariable()
         self._s = PositiveStableRandomVariable(self.alpha)
         self._laplace_transform = LinnikLaplaceTransform(alpha, beta)
 
@@ -70,11 +72,18 @@ class Linnik(RandomVariable):
     def variance(self) -> np.float64:
         return np.inf if self.alpha < 1 else self.beta
 
-    def sample(self, N: int) -> np.ndarray[float]:
-        v = self.rng.gamma(self.beta, 1, N)
-        s = self._s.sample(N)
-        return np.power(v, 1/self.alpha) * s
-    
+    def sample(self, N: int, option='stable') -> np.ndarray[float]:
+        alpha = self.alpha
+        if option=='stable':
+            v = self.rng.gamma(self.beta, 1, N)
+            s = self._s.sample(N)
+            return np.power(v, 1/alpha) * s
+        elif option == 'polya':
+            v = np.abs(self._fvp.sample(N))
+            u = self.rng.uniform(0, 1, N)
+            z = ((1+alpha - np.sqrt((1+alpha)**2 - 4 * alpha * u)) / (2 * u) - 1)**(1/alpha)
+            return v/z
+            
     def laplace_transform_kth_derivative_at_x(self, k: int, t: np.float64) -> np.float64:
         # TODO: (-1)**k * ?
         return self._laplace_transform.get_kth_derivative_at_x(k, t)
--- branching_processes_simulation/src/branching_processes_simulation/stable_random_variable.py
+++ branching_processes_simulation/src/branching_processes_simulation/stable_random_variable.py
@@ -1,16 +1,24 @@
 from typing import Any, Callable
 import numpy as np
 
+from branching_processes_simulation.constant_variable import ConstantVariable
+from branching_processes_simulation.continuous_space_process.fejer_de_la_vallee_poussin_random_variable import FejerDeLaValleePoussinRandomVariable
 from branching_processes_simulation.random_variable import RandomVariable
 
 
 # TODO: unstable near alpha=1: 
 class PositiveStableRandomVariable(RandomVariable):
+    def __new__(cls, alpha: float, d: float=1, *args, **kwargs):
+        if alpha == 1:
+            return ConstantVariable(d)
+        return super().__new__(cls)
+    
     # alpha < 1
     def __init__(self, alpha: float, d: float=1) -> None:
         assert 0 < alpha <= 1 and d > 0
         self.alpha = alpha
         self.d = d
+        self._fvp = FejerDeLaValleePoussinRandomVariable()
 
     def characteristic_function(self, t: np.complex64) -> np.complex64:
         return np.exp(- self.d * np.power(t, self.alpha))
@@ -43,10 +51,17 @@ class PositiveStableRandomVariable(RandomVariable):
         c2 = np.power(c2, self.alpha / (1 - self.alpha))
         return np.sin((1 - self.alpha) * theta) * c2 / np.power(np.sin(theta), 1/(1 - self.alpha))
 
-    def sample(self, N: int) -> np.ndarray[float]:
-        theta = self.rng.uniform(0, 1, N)
-        w = -np.log(self.rng.uniform(0, 1, N))
-        return np.power(self._a(theta) / w, (1 - self.alpha) / self.alpha) * (self.d**(1/self.alpha))
+    def sample(self, N: int, option='CMS') -> np.ndarray[float]:
+        alpha = self.alpha
+        if option=='CMS':
+            theta = self.rng.uniform(0, 1, N)
+            w = -np.log(self.rng.uniform(0, 1, N))
+            return np.power(self._a(theta) / w, (1 - alpha) / alpha) * (self.d**(1/alpha))
+        elif option == 'polya':
+            v = np.abs(self._fvp.sample(N))
+            u1, u2 = self.rng.uniform(0, 1, (2, N))
+            s = np.log(np.max(self.alpha / (u1 * u2), 1/u1)) ** (1 / alpha)
+            return v / s * (self.d**(1/alpha))
     
     def sample_function(self, N: int, theta: Callable[..., Any]) -> np.ndarray[float]:
         s = self.sample(N)
