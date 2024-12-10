b765e46fafb19ec6e08bafac8262f90fdcbff8ef
Ben Povar <ben.povar@warwick.ac.uk>
2024-12-10 23:24:54 +0000

    Special CDF sampler based on the inverse transform.

--- branching_processes_simulation/src/branching_processes_simulation/continuous_space_process/polya_transformed_tau.py
+++ branching_processes_simulation/src/branching_processes_simulation/continuous_space_process/polya_transformed_tau.py
@@ -9,7 +9,11 @@ from branching_processes_simulation.random_variable import RandomVariable
 
 
 class PolyaTransformedTau(RandomVariable):
+    _interval_a = 0
+    # _interval_b = +np.inf
+
     def __init__(self, alpha: float) -> None:
+        super().__init__()
         self.alpha = alpha
         self._x = betaprime(self.alpha, self.alpha)
 
@@ -20,17 +24,20 @@ class PolyaTransformedTau(RandomVariable):
         return None # unknown
 
     def pdf(self, y: np.float64) -> np.float64:
+        y = np.asarray(y)
         alpha = self.alpha
         ca = np.cos(np.pi * alpha / 2)
         abs_ca = np.abs(ca)
 
-        numerator = 4 * ((y**(2*alpha)*alpha + (-2*alpha - 1)*y**(4*alpha)) * abs_ca**5 +
+        numerator = 4 * y**(1-alpha) * ((y**(2*alpha)*alpha + (-2*alpha - 1)*y**(4*alpha)) * abs_ca**5 +
                         ((((-alpha + 1)*y**(2*alpha) + 3*y**(4*alpha)*(alpha + 1))*ca**2)/2 +
                         (1/2 - alpha)*y**(2*alpha) + y**(4*alpha)*(alpha + 1)) * abs_ca**3 +
                         4*((-alpha + 3/8)*y**(3*alpha) + ((alpha + 1)*y**(5*alpha))/16 +
                             y**alpha*((y**alpha)**2*alpha - alpha/16 + 1/16)) * ca**4) * alpha
         denominator = ((y**(2*alpha) + 1) * abs_ca + 2 * ca**2 * y**alpha)**3 * y
-        return numerator / denominator
+        res = numerator / denominator / y**(1-alpha)
+        res = np.where(y <= 0, 0, res)
+        return res if res.shape else res.item()
 
     def cdf(self, y: np.float64) -> np.float64:
         alpha = self.alpha
@@ -51,27 +58,37 @@ class PolyaTransformedTau(RandomVariable):
 
     def variance(self) -> np.float64:
         return np.infty
-    
-    def fractional_moment(self, beta:float) -> np.float64:
-        if beta >= self.alpha: 
-            return np.inf
-        else:
-            p1 = integrate.quad(lambda x: self.pdf(x) * x**beta, 0, 1)[0]
-            p2 = integrate.quad(
-                lambda x: x**(- beta/self.alpha) / (1 + x)**(2 + 1/self.alpha), 
-                0, 1
-            )[0]
-            return p1 + (1 + self.alpha)/self.alpha * p2 
 
     def sample(self, N: int, option='cdf', **kwargs) -> np.ndarray[float]:
         if option=='rejection_sampling':
             return None
         elif option == 'cdf':
-            def solve(u, cdf, pdf):
-                return fsolve(lambda x: cdf(x) - u, x0=1, fprime=pdf)[0]
-
-            u = self.rng.uniform(0, 1, N)
-            with ThreadPoolExecutor() as executor:  # You can also use ProcessPoolExecutor
-                u_values = list(executor.map(lambda u_i: solve(u_i, self.cdf, self.pdf), u))
-            return np.array(u_values)
+            return self.sample_from_cdf(N)
         return None
+    
+    def inverse_tail_cdf(self, x):
+        alpha = self.alpha
+        ca = np.cos(np.pi * alpha / 2)
+        abs_ca = np.abs(ca)
+
+        p = [
+                abs_ca**2*x,
+                (-abs_ca**3*alpha + 4*x*ca**2*abs_ca - abs_ca**3),
+                (4*x*ca**4 - 2*ca**4 - 2*alpha*ca**2 + 2*abs_ca**2*x - ca**2),
+                (-abs_ca**3*alpha + 4*x*ca**2*abs_ca - 3*abs_ca**3),
+                abs_ca**2*x - ca**2
+            ]
+        
+        res = np.roots(p)
+        for r in res:
+            if r.imag == 0 and r.real > 0:
+                return r.real**(1/alpha)
+        raise Exception('Undefinied behaviour!')
+
+    def sample_from_cdf(self, N, **kwargs):
+        u = self.rng.uniform(0, 1, N)
+
+        with ThreadPoolExecutor() as executor:  
+            cs = executor.map(self.inverse_tail_cdf, u)
+
+        return np.array(list(cs))
