d92331bd666c95b9a46942f6fb7238d6b47de937
Ben Povar <ben.povar@warwick.ac.uk>
2025-04-19 15:56:32 +0100

    Reworking tau sampling from unsizebiased alpha stable.

---
+++ branching_processes_simulation/src/branching_processes_simulation/unsizebiased_positive_stable_random_variable.py
@@ -0,0 +1,103 @@
+from typing import Any, Callable
+import numpy as np
+from sympy import gamma, hyp1f1, gammaincc
+
+from branching_processes_simulation.constant_variable import ConstantVariable
+from branching_processes_simulation.positive_stable_random_variable import PositiveStableRandomVariable
+
+
+class UnsizebiasedPositiveStableRandomVariable(PositiveStableRandomVariable):
+    def __new__(cls, alpha: float, d: float=1, *args, **kwargs):
+        if alpha == 1:
+            return ConstantVariable(d)
+        return super().__new__(cls)
+    
+    # alpha < 1
+    def __init__(self, alpha: float, d: float=1) -> None:
+        assert alpha <= 1
+        super().__init__(alpha, 1, d)
+
+    def characteristic_function(self, t: np.complex64) -> np.complex64:
+        return self.laplace_transform(- 1j * t)
+
+    def laplace_transform(self, t: np.float64) -> np.float64:
+        return np.exp(- self.d * np.power(t, self.alpha))
+
+    def pdf(self, x: np.float64) -> np.float64:
+        return None # unknown
+
+    def cdf(self, x: np.float64) -> np.float64:
+        return None # unknown
+
+    def mean(self) -> np.float64:
+        if self.alpha < 1:
+            return self.alpha / gamma(1/self.alpha)
+        elif self.alpha == 1:
+            return 1
+        else:
+            return None 
+
+    def variance(self) -> np.float64:
+        if self.alpha < 1:
+            return np.infty
+        else:
+            return 0
+
+    @staticmethod
+    def _F(x, a, A):
+        A = 1/A 
+
+        term1_num = (
+            A**(-1/a) * (1 + a) * x**((1 + 3 * a) / (2 * a - 2)) +
+            A**((-a - 1)/a) * x**((5 * a + 1) / (2 * a - 2)) * a
+        )
+        term1 = term1_num * gamma((a - 1)/a) * hyp1f1(2, (4*a + 1)/a, x**(a / (a - 1)) / A) * a**5 * np.exp(-x**(a / (a - 1)) / A)
+        term1 /= 12
+
+        term2_inner1 = (
+            (a + 0.5) * (1 + a) * A * x**((-1 - 3 * a) / (2 * a - 2)) +
+            x**((-a - 1) / (2 * a - 2)) * a**2 / 2
+        )
+        upper_gamma_term = gamma((1 + 2 * a)/a) * gammaincc((1 + 2 * a)/a, x**(a / (a - 1)) / A)
+        term2 = gamma((a - 1)/a) * term2_inner1 * a**2 * A * upper_gamma_term
+
+        pi_term = (
+            (a + 0.5) * A**2 * (1 + a) * x**((-1 - 3 * a) / (2 * a - 2)) +
+            a**2 * (A * x**((-a - 1)/(2 * a - 2)) - np.sqrt(x)) / 2
+        )
+        term2 -= np.pi / np.sin(np.pi / a) * (1 + a) * pi_term
+        term2 *= (a + 0.5) * A * (1/3 + a)
+
+        numerator = 12 * (-term1 + term2)
+        denominator = (
+            np.sqrt(x) * gamma((a - 1)/a) * a**2 * (2 * a**2 + 3 * a + 1) * (1 + 3 * a) * A
+        )
+
+        return numerator / denominator * A**(1/a - 1)
+
+    def _a(self, theta: np.ndarray[float]):
+        if self.alpha > 0.99:
+            sin_th = np.sin(theta)
+            cot_th = np.sqrt(1 - sin_th**2)/sin_th
+            a = 1 - self.alpha
+
+            res = a*theta \
+                - theta*(theta*cot_th + np.log(sin_th))*a**2 \
+                + (-(2*theta**3)/3 + theta*(2*theta*cot_th \
+                + np.log(sin_th) - 2)*np.log(np.sqrt(sin_th)))*a**3
+        else:
+            res = np.sin(self.alpha * theta)**self.alpha
+            res /= np.sin(theta)
+            res **= 1/self.alpha
+            res = np.sin((1 - self.alpha) * theta) * res
+        return res
+
+    def sample(self, N: int, **kwargs) -> np.ndarray[float]:
+        alpha = self.alpha
+
+        theta, U = self.rng.uniform(0, 1, (2, N))
+        A = self._a(theta * np.pi)
+
+        res = np.power(a / w, (1 - alpha) / alpha) * ((self.d)**(1/alpha))
+
+        return res
