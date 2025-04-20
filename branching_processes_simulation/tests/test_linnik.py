cdd16b37a2c411e16925138248461a0c4c6f4819
Ben Povar <ben.povar@warwick.ac.uk>
2025-04-20 20:47:23 +0100

    Test for Linnik.

---
+++ branching_processes_simulation/tests/test_linnik.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import time
+
+from scipy.integrate import quad
+from scipy.special import gamma
+
+from branching_processes_simulation.linnik import Linnik
+from branching_processes_simulation.positive_stable_random_variable import PositiveStableRandomVariable
+
+
+def test_positive_stable():
+    alpha = 0.6
+    beta = 1.7
+    S = Linnik(alpha, beta)
+
+    s1 = S.sample(500000)
+
+    x = 1
+    x0 = (1 + x**alpha)**(-beta)
+    x1 = (np.mean(np.exp(-x*s1)))
+
+    assert np.isclose(x1, x0, rtol=0.01), f"Expected {x1} to be close to {x0}"
