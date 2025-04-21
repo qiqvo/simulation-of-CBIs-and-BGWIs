0a3630f1a33ec50584b64898ae04f0d521629eba
Ben Povar <ben.povar@warwick.ac.uk>
2025-04-21 16:02:02 +0100

    Fixes.

--- branching_processes_simulation/src/branching_processes_simulation/random_variable.py
+++ branching_processes_simulation/src/branching_processes_simulation/random_variable.py
@@ -3,6 +3,7 @@ from typing import Callable
 import numpy as np
 from scipy.optimize import fsolve, root_scalar
 from concurrent.futures import ThreadPoolExecutor
+from scipy.integrate import quad
 
 from branching_processes_simulation.i_random import IRandom
 
@@ -16,34 +17,47 @@ class RandomVariable(IRandom):
 
     @abstractmethod
     def characteristic_function(self, t: np.complex64) -> np.complex64:
-        return None
+        try:
+            res = quad(lambda x: np.exp(1j * t) * self.pdf(x), self._interval_a, self._interval_b)[0]
+            return res
+        except Exception as e:
+            raise NotImplementedError()
 
     @abstractmethod
     def laplace_transform(self, t: np.float64) -> np.float64:
-        return None
+        return self.characteristic_function(1j * t)
 
     @abstractmethod
     def pdf(self, x: np.float64) -> np.float64:
-        return None
+        raise NotImplementedError()
 
     @abstractmethod
     def cdf(self, x: np.float64) -> np.float64:
-        return None
+        raise NotImplementedError()
     
     def inverse_cdf(self, x: np.float64) -> np.float64:
         return root_scalar(lambda t: self.cdf(t) - x, bracket=(self._interval_a, self._interval_b))[0]
 
     @abstractmethod
     def mean(self) -> np.float64:
-        return None
+        try:
+            res = quad(lambda x: x * self.pdf(x), self._interval_a, self._interval_b)[0]
+            return res
+        except Exception as e:
+            raise NotImplementedError()
 
     @abstractmethod
     def variance(self) -> np.float64:
-        return None
+        try:
+            res = quad(lambda x: x * x * self.pdf(x), self._interval_a, self._interval_b)[0]
+            res -= self.mean()**2
+            return res
+        except Exception as e:
+            raise NotImplementedError()
 
     @abstractmethod
     def sample(self, N: int, **kwargs) -> np.ndarray[float]:
-        return None
+        raise NotImplementedError()
 
     def sample_function(self, N: int, theta: Callable, **kwargs) -> np.ndarray[float]:
         return theta(self.sample(N, **kwargs))
@@ -85,12 +99,14 @@ class RandomVariable(IRandom):
 
     def precompute_cdf_table_linear(self, granularity=100, x0=None):
         xs = np.linspace(self._interval_a, self._interval_b, granularity, endpoint=True)[1:-1]
+
         us = self.cdf(xs)
+        
         xs = np.concatenate(([self._interval_a], xs, [self._interval_b]))
         us = np.concatenate(([0], us, [1]))
         self._table = [us, xs]
 
-    def precompute_cdf_table(self, granularity=100, precompute_table_approximation='exact'):
+    def precompute_cdf_table(self, granularity=100, precompute_table_approximation='linear', **kwargs):
         if precompute_table_approximation == 'exact':
             self.precompute_cdf_table_exact(granularity)
         elif precompute_table_approximation == 'linear':
@@ -102,7 +118,7 @@ class RandomVariable(IRandom):
                         approximation='exact',
                         **kwargs) -> np.ndarray[float]:
         if self._table is None or (self._table and len(self._table) < 2 * N and N < 1e6):
-            self.precompute_cdf_table(int(min(N, 1e6)), **kwargs)
+            self.precompute_cdf_table(int(min(N, 1e5)), **kwargs)
     
         if approximation == 'exact':
             return self._sample_from_cdf_exact(N, pdf_available)
@@ -131,16 +147,16 @@ class RandomVariable(IRandom):
                 fprime=pdf).root
         
         def parallel_solve(u):
-            i = int(u * (len(self._table) - 1))
+            i = int(u * (len(self._table[0]) - 1))
             l, r = i, i+1
-            while self.cdf(self._table[l]) > u:
+            while self.cdf(self._table[1][l]) > u:
                 l -= 1
-            while self.cdf(self._table[r]) < u:
+            while self.cdf(self._table[1][r]) < u:
                 r += 1
 
             return solve(u, 
-                    bracket=(self._table[l], self._table[r]), 
-                    x0=self._table[i],
+                    bracket=(self._table[1][l], self._table[1][r]), 
+                    x0=self._table[1][i],
                     cdf=self.cdf, 
                     pdf=self.pdf if pdf_available else None)
 
