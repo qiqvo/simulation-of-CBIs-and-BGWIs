999d975643e7d5bcfdd717bd506a8b9447b40b2a
Ben Povar <ben.povar@warwick.ac.uk>
2025-04-21 17:18:48 +0100

    Test for CB sampling.

---
+++ branching_processes_simulation/tests/test_sample_cb.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from branching_processes_simulation.continuous_space_process.stable_cb import StableCB
+
+
+def test_sample_cb():
+    alpha = 0.6
+    c = 1
+    X = StableCB(alpha, c)
+
+    t = 1
+    z = 1
+    s1 = X.sample_on_time(500000, t, z)
+
+    x = 1
+    x0 = X.laplace_transform(x, t, z)
+    x1 = (np.mean(np.exp(-x*s1)))
+
+    assert np.isclose(x1, x0, rtol=0.05), f"Expected {x1} to be close to {x0}"
