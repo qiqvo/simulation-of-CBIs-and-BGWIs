c30046b41a8ca8511ed90a9c60a964ed173dc00e
Ben Povar <ben.povar@warwick.ac.uk>
2025-04-22 15:59:48 +0100

    Added a zero truncated poisson and a test.

---
+++ branching_processes_simulation/tests/test_sample_zero_truncated_poisson.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from branching_processes_simulation.random_variable.zero_truncated_poisson import ZeroTruncatedPoisson
+
+
+def test_sample_zero_truncated_poisson():
+    rate = 4
+    X = ZeroTruncatedPoisson(rate)
+
+    s1 = X.sample(500000, option='cdf')
+    s2 = X.sample(500000, option='poisson')
+
+    x = 1
+    x0 = X.laplace_transform(x)
+    x1 = (np.mean(np.exp(-x*s1)))
+    x2 = (np.mean(np.exp(-x*s2)))
+
+    assert np.isclose(x1, x2, rtol=0.005), f"Expected {x1} to be close to {x0}"
+    assert np.isclose(x1, x0, rtol=0.005), f"Expected {x1} to be close to {x0}"
+    assert np.isclose(x2, x0, rtol=0.005), f"Expected {x2} to be close to {x0}"
