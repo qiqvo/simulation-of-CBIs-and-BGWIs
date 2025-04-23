63b8842df5e2cbd8b60b57111040bff0ad4e1d26
Ben Povar <ben.povar@warwick.ac.uk>
2025-04-23 09:49:24 +0100

    Profile sampling.

--- branching_processes_simulation/src/branching_processes_simulation/random_process/random_process.py
+++ branching_processes_simulation/src/branching_processes_simulation/random_process/random_process.py
@@ -34,14 +34,15 @@ class RandomProcess(IRandom):
     def _get_profile_times(self, time, **kwargs):
         return None
 
+    ## Returns a sample in a shape (N, len(times)), len(times) ~ t_per_1 * time
     def sample_profile(self, N: int, time: float, z: float, **kwargs) -> np.ndarray[int]:
         times = self._get_profile_times(time, **kwargs)
         m = len(times)
 
         profile = np.zeros((N, m), np.float64)
-        profile[:, 0] = z 
+        profile[:, 0] = [z] * N
         for i in range(1, m):
             dt = times[i]
-            profile[:, i] = self.sample(N, dt, profile[:, i - 1], **kwargs)
+            profile[:, i] = self.sample(1, dt, profile[:, i - 1], **kwargs)[:, 0]
 
         return profile
---
+++ branching_processes_simulation/tests/test_sample_cb_in_two.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from branching_processes_simulation.random_process.stable_cb import StableCB
+
+
+def test_sample_cb_in_two():
+    alpha = 0.6
+    c = 1
+    X = StableCB(alpha, c)
+
+    t = 1
+    z = 1
+    N = 500000
+    # s1 = X.sample(N, t, np.array([1, 2, 3, 4]) * z)
+    
+    s1 = X.sample(N, t/2, [z])
+    s2 = X.sample(1, t/2, s1[0])
+    # print(s2)
+    s3 = X.sample(N, t, [z])
+    s4 = X.sample_profile(N, t, z, t_per_1=2)[:, -1]
+
+    x = 1
+    x0 = X.laplace_transform(x, t, z)
+    x2 = (np.mean(np.exp(-x*s2)))
+    x3 = (np.mean(np.exp(-x*s3)))
+    x4 = (np.mean(np.exp(-x*s4)))
+
+    assert np.isclose(x2, x0, rtol=0.001), f"Expected {x2} to be close to {x0}"
+    assert np.isclose(x3, x0, rtol=0.001), f"Expected {x3} to be close to {x0}"
+    assert np.isclose(x4, x0, rtol=0.001), f"Expected {x2} to be close to {x0}"
